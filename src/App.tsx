b394f49fd5840431258f9f0efaa78ae4833ac8f0
Callum McLuskey <callummcluskey100@gmail.com>
2023-03-07 17:41:16 +0000

    fixed input

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
       <div style={{display:"flex", width:"100%",alignItems:"center",justifyContent:"center"}}>
 
         <div style={{display:"flex"}}>
-        <Input value={passwordInput} pr="xs"/>
+        <Input value={passwordInput} onInput={(e) => setPasswordInput((e.target as any).value)} pr="xs"/>
         <div>
         <UnstyledButton pt={7} onClick={()=>setShowEmojiPicker(!showEmojiPicker)}><AiOutlineSmile size={20} color="gray"/></UnstyledButton>
         <div style={{position:"absolute", top:50, left:"50%",transform:"translateX(-50%)"}}>
