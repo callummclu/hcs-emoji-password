10694b7204ec110e26806341c3c477b64f5e945c
Callum McLuskey <callummcluskey100@gmail.com>
2023-03-08 17:14:20 +0000

    removed .env reference

--- server/configs/env.go
+++ server/configs/env.go
@@ -1,24 +1,17 @@
 package configs
 
 import (
-	"log"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 func getEnvByName(name string) string {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("error loading .env file")
-	}
 	return os.Getenv(name)
 }
 
 func EnvPORT() string {
 
 	port := "8080"
-	env_port := getEnvByName("PORT")
+	env_port := os.Getenv("PORT")
 
 	if len(env_port) > 0 {
 		port = env_port
