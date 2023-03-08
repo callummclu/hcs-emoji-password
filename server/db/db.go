5299967fd03c099c60319ad298a32f68443429ef
Callum McLuskey <callummcluskey100@gmail.com>
2023-03-08 16:17:59 +0000

    further additions

--- server/db/db.go
+++ server/db/db.go
@@ -8,11 +8,11 @@ import (
 )
 
 func GetDB() (*sql.DB, error) {
-	host := getEnvByName("DB_HOST")
-	port := getEnvByName("DB_PORT")
-	user := getEnvByName("DB_USER")
-	password := getEnvByName("DB_PASS")
-	dbname := getEnvByName("DB_NAME")
+	host := configs.getEnvByName("DB_HOST")
+	port := configs.getEnvByName("DB_PORT")
+	user := configs.getEnvByName("DB_USER")
+	password := configs.getEnvByName("DB_PASS")
+	dbname := configs.getEnvByName("DB_NAME")
 
 	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 
