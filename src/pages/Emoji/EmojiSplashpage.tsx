15919bce1fc0864b57cb7d88dc092dd80ba326ac
Jamie Fox <2383228f@student.gla.ac.uk>
2023-03-13 19:10:13 +0000

    minor wording change

--- src/pages/Emoji/EmojiSplashpage.tsx
+++ src/pages/Emoji/EmojiSplashpage.tsx
@@ -5,7 +5,7 @@ export const EmojiSplashPage = () => {
     return (
         <>
             <Title mt="xl">Stage 1: Emoji Entry</Title>
-            <Text mt="md">In this section we will ask you to enter an emoji-based password. Use the emoji picker to the right of the input box along with the keyboard to enter your password, which must be at least 8 characters long and include emojis. </Text>
+            <Text mt="md">In this section we will ask you to enter an emoji-based password. On the next page, use the emoji picker to the right of the input box along with the keyboard to enter your password, which must be at least 8 characters long and include emojis. </Text>
 
             <Affix position={{ bottom: rem(20), right: rem(20)}}>
                 <a href="emoji/entry"><Button color="green" leftIcon={<VscDebugStart/>}>Get Started</Button></a>
